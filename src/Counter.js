031116ed42b46a996c664b08cdf07f639d439303
JAYARAJ <jayarajcs2011@hotmail.com>
2024-06-23 22:01:21 +0530

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,42 @@
+// src/Counter.js
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      count: 0,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
